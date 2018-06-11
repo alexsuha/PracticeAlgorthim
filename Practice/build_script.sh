b39eb822a069dfb7c3eca0bf46ed3eaeda432473
lustlord <alexsuha@hotmail.com>
2018-06-10 18:04:42 -0600

    change micro of compile

--- Practice/build_script.sh
+++ Practice/build_script.sh
@@ -7,6 +7,16 @@
 
 echo "Now Compile........."
 
-gcc CheckFourPoints.cpp -o CheckFourPoints -lstdc++
-chmod 777 CheckFourPoints
-./CheckFourPoints
+#gcc CheckFourPoints.cpp -o CheckFourPoints -lstdc++
+#chmod 777 CheckFourPoints
+#./CheckFourPoints
+
+function compileExec()
+{
+	file=$1
+	gcc $file.cpp -o $file -lstdc++
+	chmod 777 $file
+	./$file
+}
+
+compileExec CheckFourPoints
